cab0e8b2f2829eb3295f76b59d7b3c8888d51880
Rupanshu Janbandhu <arghhjayy@gmail.com>
2024-02-14 11:18:13 +0000

    Add boilerplate function for action on data drift

--- src/model_monitor.py
+++ src/model_monitor.py
@@ -3,6 +3,10 @@ from evidently.metric_preset import DataDriftPreset
 from evidently.report import Report
 
 
+def take_action():
+    pass
+
+
 def model_monitor():
     report = Report(
         metrics=[
@@ -14,7 +18,13 @@ def model_monitor():
     df_live = pd.read_csv("inference/input/input_13-02-2024.csv")
 
     report.run(reference_data=df_ref, current_data=df_live)
-    
+
+    report = report.as_dict()
+
+    if report["metrics"][0]["result"]["number_of_drifted_columns"] > 0:
+        print("Data drift detected!")
+        take_action()
+
     report.save_json("report.json")
     report.save_html("report.html")
 
