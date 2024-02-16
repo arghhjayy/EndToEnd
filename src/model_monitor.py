38ecdc650092360ed3d435d891427869e52cc860
Rupanshu Janbandhu <arghhjayy@gmail.com>
2024-02-16 11:56:00 +0000

    Add email alerts capability on data drift

--- src/model_monitor.py
+++ src/model_monitor.py
@@ -1,3 +1,6 @@
+import smtplib
+import tomllib
+from email.message import EmailMessage
 from time import gmtime, strftime
 
 import pandas as pd
@@ -6,12 +9,35 @@ from evidently.report import Report
 from prefect import flow
 
 
-def take_action():
-    pass
+def send_email(sender_email, sender_password, recipient_email, subject, body):
+    # Create the EmailMessage object
+    message = EmailMessage()
+    message.set_content(body)
+    message["Subject"] = subject
+    message["From"] = sender_email
+    message["To"] = recipient_email
+
+    with smtplib.SMTP("smtp.gmail.com", 587) as server:
+        server.starttls()  # Use TLS encryption
+        server.login(sender_email, sender_password)
+
+        server.send_message(message)
+
+
+def take_action(config):
+    send_email(
+        sender_email=config["sender_email"],
+        sender_password=config["sender_password"],
+        recipient_email=config["to_email"],
+        subject="Data drift detected in the model!",
+        body="""Hello,
+                Data drift has been detected on the deployed model.
+                Please check""",
+    )
 
 
 @flow(log_prints=True)
-def model_monitor():
+def model_monitor(config=None):
     report = Report(
         metrics=[
             DataDriftPreset(),
@@ -30,9 +56,10 @@ def model_monitor():
 
     if report["metrics"][0]["result"]["number_of_drifted_columns"] > 0:
         print("Data drift detected!")
-        take_action()
-
+        take_action(config=config["alert"])
 
 
 if __name__ == "__main__":
-    model_monitor()
+    with open("config.toml", "rb") as f:
+        config = tomllib.load(f)
+    model_monitor(config)
